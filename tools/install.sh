ce1d4fe7967a17a504d4d090713f767fde148446
my_name <my_email>
2026-05-10 01:57:26 +0300

    fix

--- tools/install.sh
+++ tools/install.sh
@@ -4,9 +4,8 @@ PROJ="$(cd "$(dirname "$0")/.." && pwd)"
 cd "$PROJ"
 
 [ -d .venv ] || python3 -m venv .venv
-source .venv/bin/activate
-    python -m pip install -r tools/requirements.txt
-    python -m pip install ipykernel
-    python -m ipykernel install --user --name chess \
-        --display-name "Python (chess)"
-deactivate
+
+.venv/bin/python -m pip install -r tools/requirements.txt
+.venv/bin/python -m pip install ipykernel
+.venv/bin/python -m ipykernel install --user --name chess \
+    --display-name "Python (chess)"
